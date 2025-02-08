e983d753fd61319eebafd88ac2d6ca8996c8435b
Patryk Ksiazek <globalghost1@protonmail.com>
2025-02-08 18:32:44 +0100

    Locators/Extract multiple webelements in page

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -10,7 +10,6 @@ test ("Browser Context Playwright test", async ({browser}) => {
         const page = await Context.newPage();
         await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
 
-
 });
 
 test ("Page Playwright test", async ({page}) => {
@@ -23,16 +22,20 @@ test ("Page Playwright test", async ({page}) => {
 
 });
 
-test ("Wrong Credentials", async ({page}) => {
-    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
-    //get titlte of the page - assertion
-    console.log(await page.title());
-    await page.locator("#username").fill("rahulshetty");
-    await page.locator('#password').fill('learning');
-    await page.locator('#signInBtn').click();
-    //check for error message
-    console.log(await page.locator('.alert').textContent());
-    //assertion
-    await expect(page.locator("[style*='block']")).toContainText("Incorrect username/password.");
 
-});
+test ("Wrong Credentials", async ({page}) => {
+    
+        await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+        //get titlte of the page - assertion
+        console.log(await page.title());
+        await page.locator("#username").fill("rahulshetty");
+        await page.locator('#password').fill('learning');
+        await page.locator('#signInBtn').click();
+        //check for error message
+        console.log(await page.locator('.alert').textContent());
+        //assertion
+        await expect(page.locator("[style*='block']")).toContainText("Incorrect username/password.");
+    
+    });
+
+
