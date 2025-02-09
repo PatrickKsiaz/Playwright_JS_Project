3d9fdcc1d5b29411f3a4dbc45c5ace1db326c951
Patryk Ksiazek <globalghost1@protonmail.com>
2025-02-09 20:49:39 +0100

    Dynamically Find to buy from list

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -2,6 +2,8 @@ const {test, expect} = require('@playwright/test');
 
 test ("@WC Client App Login", async ({page})=> {
 
+    const productName = "Zara Coat 4";
+    const products = page.locator(".card-body");
     await page.goto("https://rahulshettyacademy.com/client");
     await page.locator("#userEmail").fill("anshika@gmail.com")
     await page.locator("#userPassword").fill("Iamking@000")
@@ -9,7 +11,16 @@ test ("@WC Client App Login", async ({page})=> {
     //await page.waitForLoadState('networkidle');
     await page.locator("card-body b").first().waitFor();
     const titles =  await page.locator("card-body b").allTextContents();
-    
     console.log(titles);
-});
+    //Zara Coat 4
+    const count = await products.count();
+    
+    for(let i =0; i< count; i++i)
+    {
+     if(await products.nth(i).locator("b").textContent() === productName)
+    {
+        //Add to cart
+    }
+
+    }
 
